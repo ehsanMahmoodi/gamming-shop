9d226ef281da0a255d5d85665ba30acdc6a6b46a
Ehsan Mahmoodi <mahmoodie24m@gmail.com>
2022-03-29 12:16:11 +0430

    feature : handel to undefined routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import AllGames from "../pages/AllGames/AllGames";
 import Cart from "../pages/Cart/Cart";
+import Layout from "../Layout/Layout";
+import Error404 from "../components/Error404/Error404";
 
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
       <Route path={"/"} element={<Home />} />
       <Route path={"/all-games"} element={<AllGames />} />
       <Route path={"/cart"} element={<Cart />} />
+      <Route path={"*"} element={<Layout><Error404/></Layout>} />
     </Routes>
   );
 };
